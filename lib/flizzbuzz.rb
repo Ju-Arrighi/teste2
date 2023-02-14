33b889489b4fc30c8575df6bda11c980183b652d
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-02-14 21:27:33 +0100

    all tests done

--- lib/flizzbuzz.rb
+++ lib/flizzbuzz.rb
@@ -1,7 +1,22 @@
 require 'pry-byebug'
 class FlizzBuzz < Array
   def initialize
-    super ('1'..'100').to_a
-    # binding.pry
+    super array = (1..100).to_a
+    super flizz(array)
+  end
+
+  def flizz(array)
+    array2 = array.map do |n|
+      if (n % 15) == 0
+        'FlizzBuzz'
+      elsif (n % 5) == 0
+        'Buzz'
+      elsif (n % 3) == 0
+        'Blizz'
+      else
+        n
+      end
+    end
+    array2
   end
 end
--- spec/flizzbuzz_spec.rb
+++ spec/flizzbuzz_spec.rb
@@ -3,15 +3,22 @@ require 'flizzbuzz'
 describe 'FlizzBuzz' do
   subject(:output) { FlizzBuzz.new }
   it 'should print 1 as the first output' do
-    expect(subject.first).to eq('1')
+    expect(subject.first).to eq(1)
   end
 
   it 'should print 100 objects output' do
     expect(subject.size).to eq(100)
   end
-  it 'should print Flizz when a number a is multiple of 3'
 
-  it 'should print Buzz when a number a is multiple of 5'
+  it 'should print Flizz when a number a is multiple of 3' do
+    expect(subject[2]).to eq('Blizz')
+  end
 
-  it 'should print FlizzBuzz when a number a is multiple of 15'
+  it 'should print Buzz when a number a is multiple of 5' do
+    expect(subject[4]).to eq('Buzz')
+  end
+
+  it 'should print FlizzBuzz when a number a is multiple of 3 and 5' do
+    expect(subject[14]).to eq('FlizzBuzz')
+  end
 end
